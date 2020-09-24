1045d6de64ec545d0e13191c13ed6d7f3d0fcc16
Michael <michaelventuri58@aol.com>
2020-09-23 22:58:33 -0400

    Flix part 2

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Michael Venturi on 9/23/20.
+//  Copyright © 2020 Michael Venturi. All rights reserved.
+//
+
+import UIKit
+
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
